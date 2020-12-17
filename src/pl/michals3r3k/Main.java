5e9c69ebe88ceca8e9ab615ffe495ee7a3a2ecec
michals3r3k <70448532+michals3r3k@users.noreply.github.com>
2020-12-17 19:24:20 +0100

    Add files via upload

---
+++ src/pl/michals3r3k/Main.java
@@ -0,0 +1,8 @@
+package pl.michals3r3k;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/pl/michals3r3k/SnakeGame.java
@@ -0,0 +1,8 @@
+package pl.michals3r3k;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
